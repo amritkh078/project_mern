0ad89746c077c5048290a0009abd14aa55810714
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-23 21:14:43 +0545

    .

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,4 @@
-export {default as Home} from './Home';
+export {default as HomePage} from './HomePage';
 export {default as About} from './About';
 export {default as Contact} from './Contact';
 export {default as Login} from './Login';
