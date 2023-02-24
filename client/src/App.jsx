28fe2f65a1837fcbe5946295b97a4eebb7cf7615
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-24 10:07:51 +0545

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import {Navbar, Footer} from './components'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='App'>
-      Hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navbar />} />
+        <Route path='/footer' element={<Footer />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
