f3082681f9680ac75d510faaf2eb457e5dea9d42
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-28 13:45:25 +0545

    deleted

--- client/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export {default as HomePage} from './HomePage';
-export {default as About} from './About';
-export {default as Contact} from './Contact';
-export {default as Login} from './Login';
-export {default as Register} from './Register';
-export {default as Dashboard} from './Dashboard';
